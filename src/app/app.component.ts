626ee91c4fe91723624a360dd3390a2ede19b467
Kelvin Carvalho da Silva <kelvinc_silva@hotmail.com>
2018-11-25 15:38:51 -0200

    Adicionando Projeto HelpDesk Front

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from './services/shared.service';
+import { UserService } from './services/user/user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  showTemplate: Boolean = false;
+  public shared: SharedService;
+
+  constructor(private userService: UserService) {
+    this.shared = SharedService.getInstance();
+  }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+    this.shared.showTemplate.subscribe(
+      show => this.showTemplate = show
+    );
+  }
+
+  showContentWrapper() {
+    return {
+      'content-wrapper': this.shared.isLoggedIn()
+    };
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { UserService } from './services/user/user.service';
+import { routes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/security/login/login.component';
+import { SharedService } from './services/shared.service';
+import { FormsModule } from '@angular/forms';
+import { AuthInterceptor } from './components/security/auth.interceptor';
+import { AuthGuard } from './components/security/auth.guard';
+import { UserNewComponent } from './components/user-new/user-new.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { DialogService } from './dialog.service';
+import { TicketService } from './services/ticket/ticket.service';
+import { TicketNewComponent } from './components/ticket-new/ticket-new.component';
+import { TicketListComponent } from './components/ticket-list/ticket-list.component';
+import { TicketDetailComponent } from './components/ticket-detail/ticket-detail.component';
+import { SummaryComponent } from './components/summary/summary.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    UserNewComponent,
+    UserListComponent,
+    TicketNewComponent,
+    TicketListComponent,
+    TicketDetailComponent,
+    SummaryComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routes,
+
+  ],
+  providers: [
+    UserService,
+    AuthGuard,
+    DialogService,
+    SharedService,
+    TicketService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
